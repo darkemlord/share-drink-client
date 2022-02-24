7c0bc643924c57db6e43e3d082d6562ce803e35a
emanuel caro <darkemlord@gmail.com>
2022-02-24 23:35:57 +0900

    pages folder created, home pages updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Home from './Pages/Home'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello new App 💅🏻 hi</h1>
+      <Home />
     </div>
   );
 }
