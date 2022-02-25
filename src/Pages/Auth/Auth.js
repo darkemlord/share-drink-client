e7534cff16f5112843137603ddb16819e34b38cb
emanuel caro <darkemlord@gmail.com>
2022-02-25 10:59:49 +0900

    Auth page styling updated logo added

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Auth.scss';
 import RegisterForm from '../../Components/RegisterForm/RegisterForm';
 import { Container } from '@mui/material';
+import logo from '../../assets/Images/sharedrinklogo.png';
 
 
 const Auth = (props) => {
   const { setAuth } = props;
   return (
-    <Container>
+    <Container className='auth'>
+      <img className='logo-form'src={logo} alt='no available'/>
       <RegisterForm />
     </Container>
   )
