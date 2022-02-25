39aec46deea0ba8bb074d1194bad2c7e5e1d995e
emanuel caro <darkemlord@gmail.com>
2022-02-25 10:19:36 +0900

    button css style updated

--- src/Pages/Auth/Auth.js
+++ src/Pages/Auth/Auth.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Auth.scss';
+import { Button } from '@mui/material'
 
 const Auth = (props) => {
   const { setAuth } = props;
   return (
-    <div>Auth</div>
+    <div>
+      <Button variant='contained'>Click on me</Button>
+    </div>
   )
 }
 
