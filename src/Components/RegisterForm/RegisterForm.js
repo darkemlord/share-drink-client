3b30826c773cc08b4a7bbdd597eaf194b57173bb
emanuel caro <darkemlord@gmail.com>
2022-02-26 15:25:40 +0900

    register form input styles updated

--- src/Components/RegisterForm/RegisterForm.js
+++ src/Components/RegisterForm/RegisterForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, makeStyles, InputAdornment, PersonIcon } from '@mui/material';
 import './RegisterForm.scss';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
@@ -10,22 +10,32 @@ const RegisterForm = () => {
       <form onSubmit={() => console.log('hello world')}>
         <TextField
           fullWidth
+          focused={false}
           margin='normal'
           size='small'
           id='name'
           name='name'
           label='name'
-          inputProps={{className: 'input-style'}}
+          inputProps={{shrink: false}}
+          style={{borderRadius:'10px',
+            backgroundColor: "white"
+          }}
+
         />
 
         <TextField
           fullWidth
+          focused={false}
           margin='normal'
           size='small'
           id='username'
           name='username'
           label='username'
-          inputProps={{className: 'input-style'}}
+          InputProps={{shrink:false}}
+          style={{
+            borderRadius:'10px',
+            backgroundColor: "white"
+          }}
         />
         <Button variant='contained'>Click on me</Button>
       </form>
