316b42d676b834c03c271a95f8b6dd1f204670aa
emanuel caro <darkemlord@gmail.com>
2022-02-26 14:15:55 +0900

    name text input added;

--- src/Components/RegisterForm/RegisterForm.js
+++ src/Components/RegisterForm/RegisterForm.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import './RegisterForm.scss';
-import { useFormik }from 'formik';
+import { useFormik } from 'formik';
 import * as yup from 'yup';
 
 const RegisterForm = () => {
   return (
     <div>
       <form onSubmit={() => console.log('hello world')}>
-
+        <TextField
+          fullWidth
+          id='name'
+          name='name'
+          label='name'
+        />
         <Button variant='contained'>Click on me</Button>
       </form>
     </div>
