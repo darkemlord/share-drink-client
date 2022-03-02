03d87243d607bbfbd2750b7b9c55261a62a89d90
emanuel caro <darkemlord@gmail.com>
2022-03-02 22:50:40 +0900

    Login form basic styles done

--- src/Components/Auth/LoginForm/LoginForm.js
+++ src/Components/Auth/LoginForm/LoginForm.js
@@ -1,9 +1,72 @@
-import React from 'react'
+import React from 'react';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { TextField, Button } from '@mui/material';
+import './LoginForm.scss'
 
 const LoginForm = () => {
+
+    const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: yup.object({
+      email: yup.string().email('Email is not valid').required('Email is required'),
+      password: yup.string().required('Password is required')
+    }),
+    onSubmit: (formData) => {
+      console.log(formData)
+    },
+  });
   return (
-    <div>LoginForm</div>
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          fullWidth
+          focused={false}
+          margin='normal'
+          size='small'
+          id='email'
+          name='email'
+          label='Email'
+          InputProps={{shrink: +false}}
+          style={{
+            borderRadius:'10px',
+            backgroundColor: "white"
+          }}
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email && true}
+        />
+
+        <TextField
+          fullWidth
+          focused={false}
+          margin='normal'
+          size='small'
+          type='password'
+          id='password'
+          name='password'
+          label='password'
+          InputProps={{shrink: +false}}
+          style={{
+            borderRadius:'10px',
+            backgroundColor: "white"
+          }}
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password && true}
+        />
+
+        <Button className='register-button' type='submit' variant='contained'>Login</Button>
+      </form>
+    </>
   )
+}
+
+function initialValues(){
+    return {
+      email: "",
+      password: "",
+    }
 }
 
 export default LoginForm;
